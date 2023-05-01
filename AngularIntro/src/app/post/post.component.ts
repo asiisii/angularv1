1dcd88d8cb766f4d58de1db95c27b08d37a3895d
Ashish Malla <ashishmalla45@gmail.com>
2023-05-01 11:54:02 -0600

    second day notes

--- AngularIntro/src/app/post/post.component.ts
+++ AngularIntro/src/app/post/post.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  title: string = 'List of Posts';
+  postData: string = 'this is the data from the post component'; 
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
