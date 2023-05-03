b3a681f750725b1fa1e9baaba22b8602787c68a7
Ashish Malla <ashishmalla45@gmail.com>
2023-05-02 19:13:37 -0600

    add notes on ngFor directive

--- DirectivesIntro/src/app/app.component.ts
+++ DirectivesIntro/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'DirectivesIntro';
+  postArr: Array<string> = ['Post1', 'Post2', 'Post3', 'Post4', 'Post5'];
+  objArr: Array<any> = [
+    { id: 1, postTitle: 'Post1' },
+    { id: 2, postTitle: 'Post2' },
+    { id: 3, postTitle: 'Post3' },
+    { id: 4, postTitle: 'Post4' },
+    { id: 5, postTitle: 'Post5' },
+  ];
 }
