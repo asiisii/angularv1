1fd1117f1990913af1a662686d815cd02c4fe868
Ashish Malla <ashishmalla45@gmail.com>
2023-05-04 09:57:49 -0600

    add notes on ng-template directive

--- DirectivesIntro/src/app/app.component.ts
+++ DirectivesIntro/src/app/app.component.ts
@@ -9,10 +9,6 @@ export class AppComponent {
   postArr: Array<string> = ['Post1', 'Post2', 'Post3', 'Post4', 'Post5'];
   objArr: Array<any> = [
     { id: 1, postTitle: 'Post1' },
-    { id: 2, postTitle: 'Post2' },
-    { id: 3, postTitle: 'Post3' },
-    { id: 4, postTitle: 'Post4' },
-    { id: 5, postTitle: 'Post5' },
   ];
 
   emptyObjArr: Array<any> = [];
