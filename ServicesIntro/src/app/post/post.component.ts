1cdbc8ebfd80da42f2549d171ff88279a5f01845
Ashish Malla <ashishmalla45@gmail.com>
2023-05-04 20:15:11 -0600

    add notes on dependency injection and services

---
+++ ServicesIntro/src/app/post/post.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { PostService } from '../Services/post.service';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css'],
+  providers: [PostService]
+})
+export class PostComponent {
+  posts!: Array<any>;
+
+  constructor(private postService: PostService) {
+    // let postService = new PostService()
+    this.posts = postService.postList
+  }
+}
