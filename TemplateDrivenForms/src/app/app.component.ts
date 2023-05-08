97f47fcb25f396d21650c500573c077d9e8aca31
Ashish Malla <ashishmalla45@gmail.com>
2023-05-08 17:04:15 -0600

    add styling with bootstrap for template driven forms

--- TemplateDrivenForms/src/app/app.component.ts
+++ TemplateDrivenForms/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
+  onSubmit(f: NgForm) {
+    console.log('====================================');
+    console.log(f.value);
+    console.log('====================================');
+  }
+
+  getValue(f: any) {
+    console.log('====================================');
+    console.log(f);
+    console.log('====================================');
+  }
 }
